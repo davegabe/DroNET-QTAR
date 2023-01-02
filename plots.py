2676bc52d923c8e1b25b6fc8fd8c9f34e9eca855
THESHADOW2030 <hazem030500@gmail.com>
2023-01-02 19:08:01 +0100

    plot stuff and much more

---
+++ plots.py
@@ -0,0 +1,52 @@
+import json
+import matplotlib.pyplot as plt
+import os
+import time
+
+def plot_avg_results(data: list[str], data_type: str, algorithms: list[str]):
+    """Make a plot for a specific type of result (e.g. average_delivery_ratio, average_delivery_time, etc.) with all algorithms"""
+    avg = {}
+    found_algorithms = []
+    for alg in algorithms:
+        for file in data:
+            if alg in file:
+                found_algorithms.append(alg)
+                with open(f"./results/{file}") as f:
+                    file = json.load(f)
+                # Each key is number of drones
+                # Each value is a list of simulation results
+                for key in file.keys():
+                    avg[key] = sum(file[key]) / len(file[key])
+                # Plot values
+                plt.plot(list(avg.keys()), list(avg.values()))
+    # Set legend
+    plt.legend(found_algorithms)
+
+    # Set title and labels
+    plt.title("All algorithms")
+    plt.xlabel('Number of drones')
+    plt.ylabel(data_type.replace('_', ' ').capitalize())
+    # Save plot
+    plt.savefig(f"./plots/{data_type}.png", dpi=400)
+    # Close plot
+    plt.close()
+
+def main():
+    # Create folders if they don't exist
+    os.makedirs('./plots', exist_ok=True)
+    os.makedirs('./results', exist_ok=True)
+    # Get all json files from ./results folder
+    json_files = [file for file in os.listdir('./results') if file.endswith('.json')]
+
+    data_types = ['average_delivery_ratio', 'average_delivery_time', 'average_number_of_relays']
+    algorithms = ["QTAR", "GEO", "RND", "QL"]
+
+    # Plot values for each json file
+    for data_type in data_types:
+        # Get json files for each data type
+        files_data_type = [file for file in json_files if data_type in file]
+
+        plot_avg_results(files_data_type, data_type, algorithms)
+
+if __name__ == '__main__':
+    main()
--- src/experiments/experiment_ndrones.py
+++ src/experiments/experiment_ndrones.py
@@ -1,8 +1,19 @@
+import sys
 from src.utilities.experiments_config import *
 from src.experiments.parser.parser import command_line_parser
 from src.utilities import config
 from src.simulation.simulator import Simulator
 import os
+from multiprocessing import Pool
+from pprint import pprint
+import numpy as np
+import json
+from datetime import datetime
+
+OUTPUT_SUPPRESSED = True
+MULTIPROCESS = True
+ALG = "QTAR"
+MAX_PROCESSES = 16
 
 
 def sim_setup(n_drones, seed, algorithm):
@@ -47,7 +58,7 @@ def sim_setup(n_drones, seed, algorithm):
     )
 
 
-def launch_experiments(n_drones, in_seed, out_seed, algorithm):
+def launch_experiments_oneseed(args):
     """
     The function launches simulations for a given algorithm and drones number
     with seeds ranging from in_seed up to out_seed
@@ -57,31 +68,81 @@ def launch_experiments(n_drones, in_seed, out_seed, algorithm):
     @param algorithm: the routing algorithm
     @return:
     """
+    n_drones, seed, algorithm = args
+    print(f"Running {algorithm} with {n_drones} drones seed {seed}")
+    sim = sim_setup(n_drones, seed, algorithm)
+    sim.run()
+    # Metrics
+    sim.metrics.other_metrics()  # calculate other metrics
+    delivery_ratio = len(sim.metrics.drones_packets_to_depot_list) / sim.metrics.all_data_packets_in_simulation
+    mean_delivery_time = sim.metrics.event_mean_delivery_time
+    min_num_of_relays = np.nanmean(sim.metrics.mean_numbers_of_possible_relays)
+    sim.close()
+    return delivery_ratio, mean_delivery_time, min_num_of_relays
 
-    for seed in range(in_seed, out_seed):
 
-        print(f"Running {algorithm} with {n_drones} drones seed {seed}")
+def main():
+    # Define the number of drones to test
+    number_drones = [5, 10, 15, 20, 25, 30]
+    # Define the number of seeds to test
+    seeds = [i for i in range(30)]
+    # For each number of drones, launch the simulation with the given seeds
+    # save the results as a dictionary drone_number: [list of delivery ratios]
+    delivery_ratios = {number_drone: [] for number_drone in number_drones}
+    mean_delivery_times = {number_drone: [] for number_drone in number_drones}
+    min_number_relays = {number_drone: [] for number_drone in number_drones}
 
-        simulation = sim_setup(n_drones, seed, algorithm)
+    # Define the experiment parameters
+    experiments = [(n_drones, seed, ALG) for n_drones in number_drones for seed in seeds]
 
-        simulation.run()
+    # Calculate experiments
+    for i in range(0, len(experiments), MAX_PROCESSES):
+        print("Starting batch", i, "to", i + min(MAX_PROCESSES, len(experiments) - i))
+        with Pool(processes=MAX_PROCESSES, initializer=mute) as pool:
+            metrics = pool.map_async(launch_experiments_oneseed, experiments[i:i+MAX_PROCESSES]).get()
+            for j in range(len(metrics)):
+                number_drone_result = experiments[i+j][0]
+                # Extract the metrics
+                delivery_ratio = metrics[j][0]
+                mean_delivery_time = metrics[j][1]
+                min_number_relay = metrics[j][2]
+                # Save the results
+                delivery_ratios[number_drone_result].append(delivery_ratio)
+                mean_delivery_times[number_drone_result].append(mean_delivery_time)
+                min_number_relays[number_drone_result].append(min_number_relay)
 
-        simulation.close()
+    # Print the results
+    for number_drone in number_drones:
+        print("#"*20)
+        print(f"Number of drones: {number_drone}")
+        print(f"Average delivery ratio: {sum(delivery_ratios[number_drone]) / len(delivery_ratios[number_drone])}")
+        print(f"Average delivery time: {sum(mean_delivery_times[number_drone]) / len(mean_delivery_times[number_drone])}")
+        print(f"Average number of relays: {sum(min_number_relays[number_drone]) / len(min_number_relays[number_drone])}")
+        print("#"*20)
+    if min(delivery_ratios) == 0:
+        print("WARNING: some simulations failed")
+    print("#"*20)
+    print("Average (of averages) delivery ratio: ", sum([sum(delivery_ratios[number_drone]) / len(delivery_ratios[number_drone]) for number_drone in number_drones]) / len(number_drones))
+    print("Average (of averages) delivery time: ", sum([sum(mean_delivery_times[number_drone]) / len(mean_delivery_times[number_drone]) for number_drone in number_drones]) / len(number_drones))
+    print("Average (of averages) number of relays: ", sum([sum(min_number_relays[number_drone]) / len(min_number_relays[number_drone]) for number_drone in number_drones]) / len(number_drones))
+    print("#"*20)
+    # Save average delivery ratio
+    with open(f"results/average_delivery_ratio_{ALG}.json", "w") as f:
+        json.dump(delivery_ratios, f)
+    # Save average delivery time
+    with open(f"results/average_delivery_time_{ALG}.json", "w") as f:
+        json.dump(mean_delivery_times, f)
+    # Save average number of relays
+    with open(f"results/average_number_of_relays_{ALG}.json", "w") as f:
+        json.dump(min_number_relays, f)
+        
+
+
+
+def mute():
+    if OUTPUT_SUPPRESSED:
+        sys.stdout = open(os.devnull, 'w')
 
 
 if __name__ == "__main__":
-
-    args = command_line_parser.parse_args()
-
-    number_of_drones = args.number_of_drones
-    initial_seed = args.initial_seed
-    end_seed = args.end_seed
-    algorithm_routing = args.algorithm_routing
-    path_filename = config.EXPERIMENTS_DIR
-
-    # build directories for results and models
-    os.system("mkdir " + path_filename)
-
-    launch_experiments(number_of_drones, initial_seed, end_seed, algorithm_routing)
-
-    print("Simulations completed!")
+    main()
